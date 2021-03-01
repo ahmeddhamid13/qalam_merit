e93dd8e70feb338269fb675e3a56ca96a9ea734a
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2021-03-01 03:44:31 +0200

    4.5.05

--- lib/merit/generators/merit_generator.rb
+++ lib/merit/generators/merit_generator.rb
@@ -8,8 +8,10 @@ module Merit
       def inject_merit_content
         if model_exists?
           inject_into_class(model_path, class_name, "  ### END ###\n\n")
-          inject_into_class(model_path, class_name, "  has_many :created_badges, class_name: 'Merit::Badge'\n")
-          inject_into_class(model_path, class_name, "  belongs_to :sash, foreign_key: 'sash_id', class_name: 'Merit::Sash'\n")
+          inject_into_class(model_path, class_name, "  has_many :created_badges, class_name: 'Merit::Badge', :dependent => :destroy\n")
+          inject_into_class(model_path, class_name, "  has_many :assigned_badges, through: :sash, source: :badges, :dependent => :destroy\n")
+          inject_into_class(model_path, class_name, "  has_many :badges_sashes, through: :sash, source: :badges_sashes, :dependent => :destroy\n")
+          inject_into_class(model_path, class_name, "  belongs_to :sash, foreign_key: 'sash_id', class_name: 'Merit::Sash', :dependent => :destroy\n")
           inject_into_class(model_path, class_name, "  has_merit\n")
           inject_into_class(model_path, class_name, "  ### QALAM_MERIT ###\n")
         end
--- lib/merit/models/active_record/qalam_score.rb
+++ lib/merit/models/active_record/qalam_score.rb
@@ -1,18 +1,24 @@
 module Merit::Models::ActiveRecord
   class QalamScore < ActiveRecord::Base
     self.table_name = :merit_scores
-    belongs_to :sash
+    belongs_to :sash,
+              foreign_key: "sash_id", 
+              class_name: 'Merit::Sash'
     has_many :score_points,
-             dependent: :destroy,
-             class_name: 'Merit::QalamScore::Point'
+              dependent: :destroy,
+              foreign_key: "score_id",
+              class_name: 'Merit::QalamScore::Point'
 
     def points
       score_points.group(:score_id).sum(:num_points).values.first || 0
     end
 
     class Point < ActiveRecord::Base
-      belongs_to :score, foreign_key: "score_id", class_name: 'Merit::QalamScore'
-      has_one :sash, through: :score
+      self.table_name = :merit_score_points
+      belongs_to :score, 
+                  foreign_key: "score_id", 
+                  class_name: 'Merit::QalamScore'
+      has_one :sash, through: :score, source: :sash
       has_many :activity_logs,
                class_name: 'Merit::ActivityLog',
                as: :related_change
